645ecc79f9a61f901fc64b26fdb88fb362a0d7a2
Mihir Chakma <mihirchakmaths@gmail.com>
2023-11-12 22:08:19 +0530

    add math file

---
+++ js/math/math.js
@@ -0,0 +1,63 @@
+// JavaScript Operators
+
+// 1. Arithmetic Operators
+// 2. Assignment Operators
+// 3. Comparison Operators
+// 4. Logical Operators
+// 5. Type Operators
+// 6. Bitwise Operators
+// 7. String Operators
+// 8. Conditional (ternary) Operator
+
+// 1. Arithmetic Operators
+    // +	Addition
+    let x = 10, y = 5;
+    console.log(x + y); 
+
+    // -	Subtraction
+    let a = 20, b = 10;
+    console.log(a - b);
+
+    // *	Multiplication 
+    let c = 10, d = 5;
+    console.log(c * d);
+
+    // **	Exponentiation (ES2016)
+    let e = 10, f = 3;
+    console.log(e ** f);
+
+    // /	Division
+    let g = 10, h = 4;
+    console.log(g / h);
+
+    // %	Modulus (Division Remainder)
+    let i = 10, j = 3;
+    console.log(i % j);
+
+    // ++	Increment
+    let k = 10;
+    console.log(++k); // 11
+    console.log(k++); // 11
+
+    // --	Decrement
+    let l = 10;
+    console.log(--l); // 9
+    console.log(l--); // 9
+
+// 2. Assignment Operators
+// =	x = y	x = y
+// +=	x += y	x = x + y
+
+// 3. Comparison Operators
+// ==	equal to
+// ===	equal value and equal type
+// !=	not equal
+// !==	not equal value or not equal type
+// >	greater than
+// <	less than
+// >=	greater than or equal to
+// <=	less than or equal to
+// ?	ternary operator
+
+
+// 4. Logical Operators 
