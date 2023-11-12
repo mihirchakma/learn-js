b918ca12e9148d353818bbc7cecab1748072d621
Mihir Chakma <mihirchakmaths@gmail.com>
2023-11-12 22:20:38 +0530

    add mathLib.js

---
+++ js/math/mathLib.js
@@ -0,0 +1,19 @@
+// Math Library 
+// Math Properties and Methods 
+
+// Eular's Number - Math.E 
+const eular = Math.E;
+console.log(eular);
+
+// Pi - Math.PI
+const pi = Math.PI;
+console.log(pi);
+
+
+// Math Methods
+
+// Absolute Value - Math.abs() 
+const a = 6, b = -13;
+
+console.log(`Absolute value of ${a} is ${Math.abs(a)}`);
+console.log(`Absolute value of ${b} is ${Math.abs(b)}`);
