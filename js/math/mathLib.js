33676e8bbff9329e08ea1ed0048d8b8689dc0532
Mihir Chakma <mihirchakmaths@gmail.com>
2023-11-14 10:50:37 +0530

    updates

--- js/math/mathLib.js
+++ js/math/mathLib.js
@@ -17,3 +17,30 @@ const a = 6, b = -13;
 
 console.log(`Absolute value of ${a} is ${Math.abs(a)}`);
 console.log(`Absolute value of ${b} is ${Math.abs(b)}`);
+
+// Floor (Rounding Down) - Math.floor()
+const decimal = 24.936;
+const floor = Math.floor(decimal);
+console.log(`The value of floor is ${floor}.`);
+
+
+// Ceil (Rounding Up) - Math.ceil()
+console.log("The value of ceil is " + Math.ceil(decimal) + ".");
+
+// Round (Rounding to Nearest) - Math.round()
+console.log("The value of round is " + Math.round(decimal) + ".");
+
+
+// Logarithm - Math.log()
+const log = Math.log(2);
+console.log(`The value of log is ${log}.`);
+
+
+// Maximum - Math.max()
+const number = Math.max(10,50,30,90,40)
+
+
+// Minimum - Math.min()
+const minimum = Math.min(120, 190, 348, 200);
+console.log("The value of minimum is " + minimum + ".");
+
