653d9b5cc1e3614fa8806e5da0a49e63adad5426
Mihir Chakma <mihirchakmaths@gmail.com>
2023-11-15 22:36:03 +0530

    add if else

---
+++ js/if-else/comparison.js
@@ -0,0 +1,71 @@
+// Comparison Operators
+
+// == equal to - Equality operator
+console.log(5 == 5);
+
+const pears = 40 / 2;
+console.log(pears == 20);
+
+console.log(15 == 10);
+console.log(15 == '15');
+
+console.log('--------');
+
+// === equal value and equal type - Strict Equality operator
+console.log(10 === 10);
+
+const apples = 50 / 2;
+console.log(apples === 25);
+
+console.log(15 === 10);
+console.log(20 === '20');
+
+console.log('--------');
+
+// != not equal to - Inequality operator
+console.log(10 != 15);
+
+const oranges = 5 * 6;
+console.log(oranges != 30);
+
+console.log(55 != 55);
+
+console.log('--------');
+
+// !== not equal value or not equal type - Strict Inequality operator
+console.log(20 !== 30);
+
+const bananas = 10 * 2;
+console.log(bananas !== 20);
+
+console.log(120 !== 120);
+
+console.log('--------');
+
+// > greater than - Greater than operator
+console.log(20 > 10); // true
+console.log(30 > 50); // false
+
+console.log('--------');
+
+// >= greater than or equal to - Greater than or equal operator
+console.log(20 >= 10); // true
+console.log(40 >= 40); // true
+console.log(50 >= 60); // false
+
+console.log('--------');
+
+// < less than - Less than operator
+const books = 20;
+
+console.log(books < 30); // true
+console.log(books < 10); // false 
+
+console.log('--------');
+
+// <= less than or equal to - Less than or equal operator
+console.log(50 <= 100); // true
+console.log(120 <= 80); // false
+console.log(100 <= 100); // true
+
+console.log('--------');
---
+++ js/if-else/conditions.js
@@ -0,0 +1,23 @@
+// if else conditions
+
+let candy = 90;
+
+if (candy >= 100) {
+    console.log("You got a cavity!");
+} else {
+    console.log("You didn't get a cavity! Well done!");
+}
+
+
+// if, else if, else conditions 
+const candies = 80;
+
+if (candies >= 100) {
+    console.log("You got a cavity!");
+} else if (candies >= 95) {
+    console.log("You almost got a cavity. Be careful!");
+} else if (candies >= 75) {
+    console.log("You should eat less candy!");
+} else {
+    console.log("You didn't get a cavity! Nice!");
+}
