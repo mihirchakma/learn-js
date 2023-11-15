cebc1f113a60d02a6274645c0063b29da6d0d7ec
Mihir Chakma <mihirchakmaths@gmail.com>
2023-11-15 22:48:45 +0530

    update conditions.js

--- js/if-else/conditions.js
+++ js/if-else/conditions.js
@@ -1,4 +1,4 @@
-// if else conditions
+//* if else conditions
 
 let candy = 90;
 
@@ -9,7 +9,7 @@ if (candy >= 100) {
 }
 
 
-// if, else if, else conditions 
+//* if, else if, else conditions 
 const candies = 80;
 
 if (candies >= 100) {
@@ -21,3 +21,26 @@ if (candies >= 100) {
 } else {
     console.log("You didn't get a cavity! Nice!");
 }
+
+
+//* Terinary operator
+// The ternary operator is a shorthand way to represent a conditional 
+// that returns a value that you can then use to assign to a variable. 
+
+//* Syntax: condition ? expression1 : expression2; 
+
+// If the condition evaluates to true, the ternary operator returns the value of expression1.
+// If the condition evaluates to false, the ternary operator returns the value of expression2.
+
+const number = 25;
+
+const result = number > 20 ? "The number is greater than 20" : "The number is less than or equal to 20";
+console.log(result); // The number is greater than 20 
+
+let number1 = 100;
+
+const isEven = number1 % 2 === 0 ? "Yes" : "No";
+console.log("Is " + number + " an even number? " + isEven); 
+
+// The ternary operator is a great way to write simple if/else statements in a single line.
+
