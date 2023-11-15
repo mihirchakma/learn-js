fc3dacfdf1b51473e078a2577c743e3a1d6a43b6
Mihir Chakma <mihirchakmaths@gmail.com>
2023-11-15 22:59:22 +0530

    add files

---
+++ js/if-else/logical.js
@@ -0,0 +1,3 @@
+// Logical Operators
+
+// Logical operators are used to determine the logic between variables or values.
---
+++ js/if-else/switch.js
@@ -0,0 +1,22 @@
+// Switch Statement
+
+// Syntax:
+
+/* switch(expression) {
+    case x:
+      //* code block
+      break;
+    case y:
+      //* code block
+      break;
+    default:
+      //* code block
+  } */
+
+
+// The switch expression is evaluated once.
+// The value of the expression is compared with the values of each case.
+// If there is a match, the associated block of code is executed.
+// If there is no match, the default code block is executed.
+
+
