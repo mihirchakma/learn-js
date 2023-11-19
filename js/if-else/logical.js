0e5ba4501ec1f5fd39df217e7504a2fbbf5cc3d5
Mihir Chakma <mihirchakmaths@gmail.com>
2023-11-19 16:59:43 +0530

    add logical.js

--- js/if-else/logical.js
+++ js/if-else/logical.js
@@ -1,3 +1,31 @@
 // Logical Operators
 
 // Logical operators are used to determine the logic between variables or values.
+
+// And (&&) 
+const wearingShoes = true;
+const needsTying = true;
+
+if (wearingShoes && needsTying) {
+    console.log("I should tie my shoes!");
+}
+
+
+// Or (||) 
+const thereIsSnickers = false;
+const thereIsSkittles = true;
+
+if (thereIsSnickers || thereIsSkittles) {
+    console.log("I'm glad there's candy in this house!");
+}
+
+
+
+// Not (!) 
+const temparature = 50;
+const itIsHot = temparature > 70;
+
+if (!itIsHot) {
+    console.log("I will wear my boots today!");
+}
+
