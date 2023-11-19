3e2f66e9a19b42ccae10e2c1d377df1a80844b79
Mihir Chakma <mihirchakmaths@gmail.com>
2023-11-19 16:34:46 +0530

    add\update switch.js

--- js/if-else/switch.js
+++ js/if-else/switch.js
@@ -20,3 +20,22 @@
 // If there is no match, the default code block is executed.
 
 
+const color = "yellow" 
+
+switch (color) {
+  case "red":
+    console.log("I want to eat an Apple");
+    break;
+  case "yellow":
+    console.log("I want to eat a Banana");
+    break;
+  case "orange":
+    console.log("I want to eat an Orange");
+    break;
+  case "green":
+    console.log("I want to eat a Pear");
+    break;
+  default:
+    console.log("I want to eat Grapes")
+}
+
