60c2e1e4a979d93c49aed5ea50fa0fb7a4001ea9
Mihir Chakma <mihirchakmaths@gmail.com>
2023-11-19 19:55:16 +0530

    add loops

---
+++ js/loops/loops.js
@@ -0,0 +1,28 @@
+
+// for loop 
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+
+// while loop 
+let i = 1;
+
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+
+
+// do while loop 
+let doritos = 100;
+
+do {
+    console.log("With " + doritos + " Doritos left, I can eat.");
+    doritos -= 20 // doritos = doritos - 20 
+} while (doritos > 0);
+
+console.log("My doritos are gone now. I am sad.😔");
+
+
+// for in loop 
