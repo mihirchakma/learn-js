8f97aa91099083bbb294bf9d9259594984bc9b1d
Mihir Chakma <mihirchakmaths@gmail.com>
2023-11-21 22:59:23 +0530

    add functions

---
+++ js/functions/function.js
@@ -0,0 +1,13 @@
+// function 
+function intro() {
+    console.log("This is a function!");
+}
+
+// arrow function 
+const introduction = () => {
+    console.log("Hello, my name is JavaScript.");
+}
+
+intro()
+introduction()
+
