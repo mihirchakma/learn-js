80f8907c73f262fefbf09f64d849edcfe3f10c3e
Mihir Chakma <mihirchakmaths@gmail.com>
2023-11-28 21:22:37 +0530

    add arrays

---
+++ js/arrays/array.js
@@ -0,0 +1,13 @@
+// Creating an array 
+const drinks = ["Water", "Juice", "Milk", "Soda"];
+
+// Retrieval 
+console.log(drinks[0]);
+
+for (let i = 0; i < drinks.length; i++) {
+    console.log(drinks[i]);
+}
+
+// forEach method and arrow functions 
+drinks.forEach((drink) => console.log(drink));
+
