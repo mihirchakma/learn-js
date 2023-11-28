2a5165c765093c6ef7009453c9daf9ab9003edaa
Mihir Chakma <mihirchakmaths@gmail.com>
2023-11-28 21:35:13 +0530

    update array.js

--- js/arrays/array.js
+++ js/arrays/array.js
@@ -8,6 +8,30 @@ for (let i = 0; i < drinks.length; i++) {
     console.log(drinks[i]);
 }
 
+// document.getElementById("drinks").innerHTML = drinks;
+
 // forEach method and arrow functions 
 drinks.forEach((drink) => console.log(drink));
+
+
+// Reassignment 
+drinks[2] = "Coffee"; // change Milk to Coffee 
+
+console.log(drinks);
+
+
+// Sort arrays
+const sortedDrinks = drinks.sort();
+
+console.log(drinks);
+
+
+// sort an array of numbers numerically 
+const numbers = [31, 99, 2, 6, 27, 64];
+
+numbers.sort(function(a, b) {
+    return a - b;
+});
+
+console.log(numbers);
 
