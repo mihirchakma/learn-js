76e01e7cb61a70668ac2529252f4771abb80dbb2
Mihir Chakma <mihirchakmaths@gmail.com>
2023-11-29 22:46:48 +0530

    update array.js

--- js/arrays/array.js
+++ js/arrays/array.js
@@ -36,21 +36,21 @@ numbers.sort(function(a, b) {
 console.log(numbers);
 
 
-// push - push() 
-const drinks1 = ["water", "juice", "milk", "soda"];
+// Push - push() 
+const drinks_1 = ["water", "juice", "milk", "soda"];
 
-drinks1.push("Tea"); // add "tea" to the end of this array 
+drinks_1.push("Tea"); // add "tea" to the end of this array 
 
-console.log(drinks1);
-
-
-// pop - pop() 
-drinks1.pop(); // remove the last item in this array 
-
-console.log(drinks1);
+console.log(drinks_1);
 
 
-// shift - shift() 
+// Pop - pop() 
+drinks_1.pop(); // remove the last item in this array 
+
+console.log(drinks_1);
+
+
+// Shift - shift() 
 const chocolates = ["Snickers", "Kit-Kat", "Twix"];
 
 const snack = chocolates.shift();
@@ -59,7 +59,7 @@ console.log(snack);
 console.log(chocolates);
 
 
-// unshift - unshift() 
+// Unshift - unshift() 
 const chocolates_1 = ["Snickers", "Kit-Kat", "Twix"];
 
 const newLength = chocolates_1.unshift("Milky Way", "3 Musketeers");
@@ -67,3 +67,28 @@ const newLength = chocolates_1.unshift("Milky Way", "3 Musketeers");
 console.log(newLength);
 console.log(chocolates_1);
 
+
+// Concat - concat() 
+const healthyDrinks = ["water", "milk"];
+const unhealthyDrinks = ["juice", "soda"];
+
+// concatenating two existing arrays 
+const allDrinks = healthyDrinks.concat(unhealthyDrinks);
+
+console.log(allDrinks);
+
+
+// Slice - slice() 
+const colors = ["red", "green", "blue", "white", "purple"];
+
+const slice = colors.slice(1, 4);
+console.log(slice);
+
+// If you only provide one parameter, 
+// it will use that number as the starting point, 
+// and copy the rest of the array
+const slice_1 = colors.slice(3);
+console.log(slice_1);
+
+const slice_2 = colors.slice(-3);
+console.log(slice_2);
