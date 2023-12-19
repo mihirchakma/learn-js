4b20a8f2721489c25fa691bd97e15189212f4970
Mihir Chakma <mihirchakmaths@gmail.com>
2023-12-19 21:52:29 +0530

    add objects file

---
+++ js/objects/objects.js
@@ -0,0 +1,13 @@
+const dog = {
+    name: "Bruno",
+    age: 5,
+    bark: function() {
+        console.log("WOOF WOOF!");
+    }
+};
+
+console.log(dog.name);
+console.log(dog.age);
+
+dog.bark();
+
