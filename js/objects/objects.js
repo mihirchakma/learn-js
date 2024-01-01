0c6b0ff653063f82da7c9ce730cba43a88e6bf8b
Mihir Chakma <mihirchakmaths@gmail.com>
2024-01-01 22:28:28 +0530

    updated objects.js

--- js/objects/objects.js
+++ js/objects/objects.js
@@ -11,3 +11,14 @@ console.log(dog.age);
 
 dog.bark();
 
+
+// Re-assigning properties
+dog.age = 7;
+dog.weight = 20;
+
+console.log(`Dog age: ${dog.age}`);
+console.log(`Dog weight: ${dog.weight}`);
+
+console.log(dog);
+
+
