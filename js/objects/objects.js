03c68a6b25cb306870474ef039b3b2f47e2504e5
Mihir Chakma <mihirchakmaths@gmail.com>
2024-01-01 22:53:38 +0530

    updated objects.js

--- js/objects/objects.js
+++ js/objects/objects.js
@@ -22,3 +22,35 @@ console.log(`Dog weight: ${dog.weight}`);
 console.log(dog);
 
 
+const cat = {
+    name: "Fluffy",
+    age: 3,
+    bark: function() {
+        console.log("MEOW MEOW!");
+    }
+};
+
+
+// HasOwnProperty - to check if a property exists in an object 
+console.log(`Has cat's name? ${cat.hasOwnProperty("name")}`);
+console.log(`Has cat's weight? ${cat.hasOwnProperty("weight")}`);
+
+
+// Key - to check all the keys in an object
+console.log(Object.keys(cat));
+
+
+// Values - to check all the values in an object
+console.log(Object.values(cat));
+
+
+// Entries - to check all the entries in an object
+const lion = {
+    name: "Cooper",
+    age: 3,
+    bark: function () {
+        console.log("ROAR ROAR");
+    }
+};
+
+
