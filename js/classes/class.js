8d69149d478c88ed801f1d7379542c6a3d2fe28c
Mihir Chakma <mihirchakmaths@gmail.com>
2024-01-02 23:39:08 +0530

    update class.js

--- js/classes/class.js
+++ js/classes/class.js
@@ -1,7 +1,17 @@
 class Pet {
 
+    constructor(name, weight, price) {
+        this.name = name;
+        this.weight = weight;
+        this.price = price;
+    }
+
+    printInfo() {
+        return `Name: ${this.name}, Weight: ${this.weight}, Price: ${this.price}`;
+    }
 }
 
-const pet = new Pet();
+const pet = new Pet("Fluffy", 20, 2000);
 console.log(pet);
+console.log(pet.printInfo());
 
