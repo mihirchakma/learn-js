05e2a4cd566d10a30a9ce9ef8ec8c84ce2cf198e
Mihir Chakma <mihirchakmaths@gmail.com>
2024-01-05 22:22:00 +0530

    update class.js

--- js/classes/class.js
+++ js/classes/class.js
@@ -11,7 +11,13 @@ class Pet {
     }
 }
 
-const pet = new Pet("Fluffy", 20, 2000);
-console.log(pet);
-console.log(pet.printInfo());
+const pet1 = new Pet("Fluffy", 20, 2000);
+// console.log(pet);
+console.log(pet1.printInfo());
+
+const pet2 = new Pet("Oreo", 10, 1000);
+console.log(pet2.printInfo());
+
+const pet3 = new Pet("Buddy", 30, 3000);
+console.log(pet3.printInfo());
 
