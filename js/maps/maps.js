1eca122911d69c5c7a60d4fea38f9ea9dbed9fcd
Mihir Chakma <mihirchakmaths@gmail.com>
2024-01-12 22:50:52 +0530

    update maps.js

--- js/maps/maps.js
+++ js/maps/maps.js
@@ -1,9 +1,10 @@
 
 const map = new Map();
 
-map.set("red", "apple");
+map.set("red", "apple"); // lost old value "red" => "apple"
 map.set("yellow", "banana");
 map.set("green", "melon");
+map.set("red", "strawberry");
 
 console.log(map);
 
