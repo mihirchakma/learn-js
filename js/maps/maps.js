dbeb66626fc67a2e68c3ffb95c6ae1dba6acecd1
Mihir Chakma <mihirchakmaths@gmail.com>
2024-01-12 23:08:17 +0530

    update maps.js

--- js/maps/maps.js
+++ js/maps/maps.js
@@ -8,3 +8,27 @@ map.set("red", "strawberry");
 
 console.log(map);
 
+// Get value by key
+const item = map.get("red");
+
+console.log(item);
+
+
+// getting size of map 
+console.log(map.size);
+
+
+// has method to check if key exists
+console.log(map.has("red"));
+console.log(map.has("yellow"));
+console.log(map.has("blue")); // ther is no blue key, so it returns false
+
+// to delete key and value, we use delete method
+map.delete("green");
+
+console.log(map);
+
+// clear method to delete all keys and values
+map.clear();
+
+console.log(map);
